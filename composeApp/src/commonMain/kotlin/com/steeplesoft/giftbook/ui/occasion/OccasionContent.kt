f14c80440c6f64cafc27bd3782ab756084e1cc96
Jason Lee <jason@steeplesoft.com>
2025-02-27 00:46:12 -0600

    Composable structure cleanup
    Removed nested scaffold
    Dummy up FAB composable
    Introduce addEditHeader composable to encapsulate label and edit/delete buttons

--- composeApp/src/commonMain/kotlin/com/steeplesoft/giftbook/ui/occasion/OccasionContent.kt
+++ composeApp/src/commonMain/kotlin/com/steeplesoft/giftbook/ui/occasion/OccasionContent.kt
@@ -2,24 +2,30 @@
 
 package com.steeplesoft.giftbook.ui.occasion
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -45,89 +51,105 @@ fun occasionView(
     val showDialog = remember { mutableStateOf(false) }
     val status by component.requestStatus.subscribeAsState()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        "Occasion Details",
-                        fontWeight = FontWeight.Bold, fontSize = 30.sp
-                    )
-                },
-                actions = {
-                    IconButton(
-                        colors = IconButtonColors(
-                            containerColor = secondaryColor,
-                            contentColor = Color.Black,
-                            disabledContainerColor = mainColor,
-                            disabledContentColor = Color.Black
-                        ),
-                        onClick = {
-                            component.edit()
-                        }) {
-                        Icon(Icons.Filled.Edit, contentDescription = "Edit")
+    if (showDialog.value) {
+        ConfirmationDialog(
+            onDismissRequest = { showDialog.value = false },
+            onConfirmation = {
+                showDialog.value = false
+                component.delete()
+            },
+            dialogTitle = "Confirmation",
+            dialogText = "Are you sure you want to delete ${occasion.name}?",
+            icon = Icons.Default.QuestionMark
+        )
+    }
+    asyncLoad(status) {
+        FAB()
+        LazyColumn(modifier = Modifier.padding(10.dp)) {
+            item {
+                addEditHeader(
+                    label = "Occasion Details",
+                    editClick = { component.edit() },
+                    deleteClick = { showDialog.value = true }
+                )
+            }
+            item {
+                Text(buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Name: ")
                     }
-                    IconButton(
-                        colors = IconButtonColors(
-                            containerColor = secondaryColor,
-                            contentColor = Color.Black,
-                            disabledContainerColor = mainColor,
-                            disabledContentColor = Color.Black
-                        ),
-                        onClick = {
-                            showDialog.value = true
-                        }) {
-                        Icon(Icons.Filled.Delete, contentDescription = "Delete")
+                    append(occasion.name)
+                })
+            }
+            item {
+                Text(buildAnnotatedString {
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("Date: ")
                     }
-                }
-            )
-        }
-    ) { paddingValues ->
-        if (showDialog.value) {
-            ConfirmationDialog(
-                onDismissRequest = { showDialog.value = false },
-                onConfirmation = {
-                    showDialog.value = false
-                    component.delete()
-                },
-                dialogTitle = "Confirmation",
-                dialogText = "Are you sure you want to delete ${occasion.name}?",
-                icon = Icons.Default.QuestionMark
-            )
-        }
-        asyncLoad(status) {
-            LazyColumn(
-                modifier = Modifier.padding(paddingValues)
-                    .padding(start = 20.dp, end = 20.dp)
-            ) {
-                item {
-                    Text(buildAnnotatedString {
-                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                            append("Name: ")
-                        }
-                        append (occasion.name)
-                    })
-                }
-                item {
-                    Text(buildAnnotatedString {
-                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                            append("Date: ")
-                        }
-                        append (occasion.eventDate.format(LocalDate.Formats.ISO))
-                    })
-                }
-                item {
-                    Text(
-                        "Recipients:",
-                        fontWeight = FontWeight.Bold, fontSize = 24.sp
-                    )
-                }
-                component.recips?.let { recips ->
-                    items(recips.recipient) { recip ->
-                        Text(text = recip.name)
-                    }
+                    append(occasion.eventDate.format(LocalDate.Formats.ISO))
+                })
+            }
+            item {
+                Text(
+                    "Recipients:",
+                    fontWeight = FontWeight.Bold, fontSize = 24.sp
+                )
+            }
+            component.recips?.let { recips ->
+                items(recips.recipient) { recip ->
+                    Text(text = recip.name)
                 }
             }
         }
+    }
+
+}
+
+@Composable
+fun addEditHeader(
+    label: String,
+    editClick: () -> Unit,
+    deleteClick: () -> Unit
+) {
+    val colors = IconButtonColors(
+        containerColor = secondaryColor,
+        contentColor = Color.Black,
+        disabledContainerColor = mainColor,
+        disabledContentColor = Color.Black
+    )
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Column(modifier = Modifier.fillMaxWidth().weight(.5f)) {
+            Text(label, fontWeight = FontWeight.Bold, fontSize = 30.sp)
+        }
+        Column {
+            IconButton(colors = colors, onClick = editClick) {
+                Icon(Icons.Filled.Edit, contentDescription = "Edit")
+            }
+        }
+        Column {
+            IconButton(colors = colors, onClick = deleteClick) {
+                Icon(Icons.Filled.Delete, contentDescription = "Delete")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun FAB() {
+    Box(
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+    ) {
+        Row(modifier = Modifier.align(Alignment.BottomEnd)) {
+            FloatingActionButton(
+                containerColor = secondaryColor,
+                onClick = {
+                    /*do something*/
+                },
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Add Button")
+            }
+        }
+
     }
 }
