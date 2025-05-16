852b043cf09d33a1ccb92a2d1146319dc552c210
Jason Lee <jason@steeplesoft.com>
2025-05-16 13:47:02 -0500

    Clean up unused code

--- composeApp/src/commonMain/kotlin/com/steeplesoft/giftbook/Constants.kt
+++ composeApp/src/commonMain/kotlin/com/steeplesoft/giftbook/Constants.kt
@@ -8,18 +8,3 @@ import kotlinx.datetime.toLocalDateTime
 internal const val TAG = "GIFTBOOK"
 
 fun LocalDate.Companion.now() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
-
-/*
-fun Int.toDollars() : String {
-    val a = this
-
-    val left = this.toInt()
-    val right = (this - left) * 100
-
-    val dollars = a - a.toInt()
-    val fp = dollars * 10f.pow(dollars.toString().length - 2)
-    val cents = "${right.toInt()}00".substring(0,2)
-
-    return "${left}.$cents"
-}
-*/
